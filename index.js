e40d6530ac448d5cb44c60da64acb4677487f717
Ariel <arielgonzalezmaillard@gmail.com>
2026-08-18 19:28:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,27 @@ import crypto from "crypto";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-// 👉 middleware para leer JSON (IMPORTANTE para webhooks)
 app.use(express.json());
-app.post("/tn/webhook", (req, res) => {
+
+// Webhook de notificaciones de MercadoLibre
+app.post("/ml/webhook", (req, res) => {
   console.log("✅ WEBHOOK RECIBIDO");
   console.log("Headers:", req.headers);
   console.log("Body:", req.body);
   return res.sendStatus(200);
 });
 
-// ======================
-// ENDPOINT DE PRUEBA (ya lo tenías)
+// Endpoint de prueba
 app.get("/", (req, res) => {
-  res.send("TN Stock Alert OK");
+  res.send("Proyecto ML - Mensajes Automáticos OK");
 });
+
 app.get("/health", (req, res) => res.send("ok"));
 
-const ML_APP_ID = process.env.TN_APP_ID;
-const ML_CLIENT_SECRET = process.env.TN_CLIENT_SECRET;
-const ML_STORE_ID = process.env.TN_STORE_ID;
-const ML_ACCESS_TOKEN = process.env.TN_ACCESS_TOKEN;
+const ML_APP_ID = process.env.ML_APP_ID;
+const ML_CLIENT_SECRET = process.env.ML_CLIENT_SECRET;
+const ML_ACCESS_TOKEN = process.env.ML_ACCESS_TOKEN;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
